4f2ca47af0e60694ef1e760b8c4ae87527e64d2e
Wallery <Wallery@mail.ru>
2024-11-08 16:26:18 +0300

    08.11.2024 Добавление module_2_hard.py

--- module_2_hard.py
+++ module_2_hard.py
@@ -1,1 +1,16 @@
+import random
 
+n = random.randint(3, 20) # Генерируем случайное число для первой ячейки
+list_key = list(range(1,n)) # Создаём список значений для ключа
+result = [] # Создаём переменную для хранения ключа
+number = 0 # Задаём начальное значение первой цифры в списке
+#for i in list_key:
+while number < n/2-1: # Пока не достигли середины списка. Далее идут повторы пар
+    for i in range(n-2):
+#    while number >= i:
+        if n % (list_key[number] + list_key[i+1]) == 0:
+            result.extend([list_key[number], list_key[i+1]])
+    number = number + 1
+
+print(f'В первом поле цифра {n}')
+print('Пароль: ', *result)
