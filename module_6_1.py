bf10ad25f80377ca18637de2c6cb23f0ca6a0b24
Wallery <Wallery@mail.ru>
2024-12-16 22:04:25 +0300

    16.12.2024 Домашняя работа "Множественное наследование"

--- module_6_1.py
+++ module_6_1.py
@@ -1,24 +1,30 @@
+import random
+
 class Animal:
     live = True
     sound = None
     _DEGREE_OF_DANGER = 0
 
     def __init__(self, speed):
-        _cords = [0, 0, 0]
+        self._cords = [0, 0, 0]
         self.speed = speed
 
     def move(self, dx, dy, dz):
-        pass
+        self._cords[0] = dx * self.speed
+        self._cords[1] = dy * self.speed
+        if dz * self.speed < 0:
+            print("It's too deep, i can't dive :(")
+        else:
+            self._cords[2] = dz * self.speed
 
     def get_cords(self):
-        print(f'X, {_cords[0]}, Y {_cords[1]}, Z {_cords[2]}')
+        print(f'X, {self._cords[0]}, Y {self._cords[1]}, Z {self._cords[2]}')
 
     def attack(self):
-        _DEGREE_OF_DANGER = super()._DEGREE_OF_DANGER
-        if super()._DEGREE_OF_DANGER > 5:
-            print("Sorry, i'm peaceful :)")
-        else:
+        if self._DEGREE_OF_DANGER >= 5:
             print("Be careful, i'm attacking you 0_0" )
+        else:
+            print("Sorry, i'm peaceful :)")
 
     def speak(self):
         print(f'{self.sound}')
@@ -27,29 +33,31 @@ class Bird(Animal):
     beak = True
 
     def lay_eggs(self):
-        print (f'Here are(is) {round(1,4)} eggs for you')
+        print (f'Here are(is) {random.randint(1,4)} eggs for you')
 
 class AquaticAnimal(Animal):
     _DEGREE_OF_DANGER = 3
 
     def dive_in(self, dz):
-        pass
+        self._cords[2] -= int(abs(self.speed / 2 * dz))
 
 class PoisonousAnimal(Animal):
     _DEGREE_OF_DANGER = 8
 
 class Duckbill(Bird, PoisonousAnimal, AquaticAnimal):
     sound = "Click-click-click"
-#    _DEGREE_OF_DANGER = super.__init__(_DEGREE_OF_DANGER)
+
 
 db = Duckbill(10)
 print(db.live)
 print(db.beak)
 db.speak()
-#db.attack()
+db.attack()
 db.move(1, 2, 3)
 db.get_cords()
 db.dive_in(6)
 db.get_cords()
 db.lay_eggs()
+
+
 
