8d7ec9b0eb857fedebf57b490c1707a05b054fa7
Wallery <Wallery@mail.ru>
2024-12-20 17:08:21 +0300

    20.12.2024 Домашнее задание по теме "Оператор "with".

--- module7/module_7_3.py
+++ module7/module_7_3.py
@@ -24,13 +24,13 @@ get_all_words - подготовительный метод, который во
 в списке слов этого файла.
 """
 class WordsFinder:
-    file_names = []
+
     def __init__(self, *args):
+        self.file_names = []
         for i in args:
             self.file_names.append(i)
 
     def get_all_words(self):
-        import string
         self.all_words = {}
         for i in self.file_names:
             with open(i, encoding = 'utf-8') as file:
@@ -42,14 +42,22 @@ class WordsFinder:
                     replace('(','').replace(')','').lower()
                     line_.extend(line.split())
             self.all_words[i] = line_
+
         return self.all_words
-                    #string.punctuation
+
     def find(self, word):
-        pass
+        return_ = ''
+        for i in self.file_names:
+            if word.lower() in self.all_words[i]:
+                return_ += (f'{i}:{self.all_words[i].index(word.lower()) + 1}\n')
+        return return_
 
     def count(self, word):
-        pass
-
+        return_ = ''
+        for i in self.file_names:
+            if word.lower() in self.all_words[i]:
+                return_ += (f'{i}:{self.all_words[i].count(word.lower())}\n')
+        return return_
 """
 В методах find и count пользуйтесь ранее написанным методом get_all_words для получения названия файла и списка его слов.
 Для удобного перебора одновременно ключа(названия) и значения(списка слов) можно воспользоваться методом словаря - item().
@@ -57,8 +65,13 @@ class WordsFinder:
 for name, words in get_all_words().items():
   # Логика методов find или count
 """
-
-finder2 = WordsFinder('test_file.txt', 'test_file1.txt','test_file2.txt', 'test_file3.txt')
+finder2 = WordsFinder('test_file.txt')
 print(finder2.get_all_words()) # Все слова
 print(finder2.find('TEXT')) # 3 слово по счёту
-#print(finder2.count('teXT')) # 4 слова teXT в тексте всего
+print(finder2.count('teXT')) # 4 слова teXT в тексте всего
+
+# Свои проверки для нескольких файлов
+finder3 = WordsFinder('test_file.txt', 'test_file1.txt', 'test_file2.txt', 'test_file3.txt')
+print(finder3.get_all_words()) # Все слова
+print(finder3.find('TEXT')) # 3 слово по счёту в каждом файле
+print(finder3.count('teXT')) # 4 слова teXT в тексте всего в каждом файле
