201c0a3cd0d5819fb6f0969cdc558aecad894695
wallery <wallery@mail.ru>
2024-12-26 16:36:28 +0300

    28.12.2024 home work

--- module_8/module_8_4.py
+++ module_8/module_8_4.py
@@ -17,7 +17,36 @@
 Также в numbers может быть записана не коллекция, а другие типы данных, например числа. Обработайте исключение TypeError
 выводя строку 'В numbers записан некорректный тип данных'. В таком случае функция просто вернёт None.
 """
-    def personal_sum(numbers):
-        pass
-    def calculate_average(numbers):
-        pass
+
+def personal_sum(numbers):
+    result = 0
+    incorrect_data = 0
+    for i in numbers:
+        try:
+            result += i
+        except TypeError:
+            incorrect_data += 1
+            print(f'Некорректный тип данных для подсчёта суммы - {i}' )
+    return [result, incorrect_data]
+
+def calculate_average(numbers):
+    try:
+        result_sum = personal_sum(numbers)
+        sum_numbers = len(numbers) - result_sum[1]
+        incorrect_data = result_sum[1]
+        result_sum = result_sum[0] / sum_numbers
+    except TypeError:
+        print ('В numbers записан некорректный тип данных')
+        return
+    except ZeroDivisionError:
+        print('Все переданные некоректны!')
+        return 0
+    print(f'Всего некорректных данных для подсчёта суммы - {incorrect_data}')
+    return result_sum
+
+
+
+print(f'Результат 1: {calculate_average("1, 2, 3")}') # Строка перебирается, но каждый символ - строковый тип
+print(f'Результат 2: {calculate_average([1, "Строка", 3, "Ещё Строка"])}') # Учитываются только 1 и 3
+print(f'Результат 3: {calculate_average(567)}') # Передана не коллекция
+print(f'Результат 4: {calculate_average([42, 15, 36, 13])}') # Всё должно работать
